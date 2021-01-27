bd92163eb2c9d694158131b3e5f1badf21f3bde3
Yassine Baghdadi <yassine.baghdadi.deve@gmail.com>
2021-01-27 10:25:24 +0100

    v 1.2 ready

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
+#
+# import urllib.request, json
+# with urllib.request.urlopen("https://raw.githubusercontent.com/YassineBaghdadi/apps_alowing/main/hadefGazAbssManager.json") as url:
+#     data = json.loads(url.read().decode())
+#     print(data)
+
+import base64
 import requests
 
-try:
-    r = requests.get('http://172.217.168.164')
-except requests.exceptions.ConnectionError as e:
-    print(e)
+
+url = 'https://api.github.com/repos/YassineBaghdadi/apps_alowing/contents/hadefGazAbssManager.json'
+req = requests.get(url)
+
+req = req.json()  # the response is a JSON
+    # req is now a dict with keys: name, encoding, url, size ...
+    # and content. But it is encoded with base64.
+# content = base64.decodestring(req)
+print(req)
